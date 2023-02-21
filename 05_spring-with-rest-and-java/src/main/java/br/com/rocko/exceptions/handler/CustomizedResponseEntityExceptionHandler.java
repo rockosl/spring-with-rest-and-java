7b65472936568cb41be413d2731030a52465b335
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-02-21 11:34:40 -0300

    refactor crud

--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.rocko.exceptions.ExceptionResponse;
-import br.com.rocko.exceptions.UnsupportedMathOperationMethod;
+import br.com.rocko.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -26,13 +26,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-	@ExceptionHandler(UnsupportedMathOperationMethod.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
 
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
 
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
 }
--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/services/PersonServices.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/services/PersonServices.java
@@ -1,69 +1,66 @@
 package br.com.rocko.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.rocko.exceptions.ResourceNotFoundException;
 import br.com.rocko.model.Person;
+import br.com.rocko.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
 	
 	public List<Person> findAll() {
 		logger.info("Finding all people!");
-		List<Person> persons = new ArrayList<>();
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		return persons ;
+		return repository.findAll() ;
 	}
 
-	public Person findByid(String id) {
+	public Person findByid(Long id) {
 		
 		logger.info("Finding one person!");
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Ricardo");
-		person.setLastName("Baschirotto");
-		person.setAddress("São Ludgero - Santa Catarina - Brasil");
-		person.setGender("Male");
-		return  person;
+		
+		return  repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 	}
 	
 	public Person create(Person person) {
 		
 		logger.info("Creating one person!");
 		
-		return person;
+		return repository.save(person);
 	}
 	
 	public Person update(Person person) {
 		
 		logger.info("Updating one person!");
 		
-		return person;
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getAddress());
+		
+		return repository.save(person);
 	}
 	
-	public void delete(String id) {
+	public void delete(Long id) {
 		
 		logger.info("Deleting one person!");
 		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+		
 	}
 	
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name " + i);
-		person.setLastName("Last name " + i);
-		person.setAddress("Some address somewhere " + i);
-		person.setGender("Gender " + i);
-		return  person;
-	}
 }

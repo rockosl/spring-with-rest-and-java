c05c2b24e32cbd0416fa3bd3f4d896e53b74f279
Ricardo <rockobaschirotto@gmail.com>
2023-02-22 17:00:27 -0300

    arrumado migration com flyway

---
+++ 05_spring-with-rest-and-java/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.person (
+    id bigint NOT NULL,
+    first_name character varying(80) NOT NULL,
+    last_name character varying(80) NOT NULL,
+    address character varying(100) NOT NULL,
+    gender character varying(6) NOT NULL
+);
---
+++ 05_spring-with-rest-and-java/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (1, 'São Ludgero', 'Ricardo', 'Male', 'Baschirotto');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (2, 'Joinville', 'Thais', 'Female', 'Baschirotto');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (3, 'Bom Retiro', 'Clara', 'Female', 'Soethe');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (4, 'São Ludgero', 'João Antônio', 'Male', 'Loch');

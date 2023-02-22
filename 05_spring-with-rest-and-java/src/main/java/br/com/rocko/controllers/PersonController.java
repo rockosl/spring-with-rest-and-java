bf480ac130bb958e2a25d85762e6cda5b413eb4c
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-02-21 21:17:09 -0300

    adicionado mapper e testes unit

--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/controllers/PersonController.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/controllers/PersonController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.rocko.model.Person;
+import br.com.rocko.data.vo.v1.PersonVO;
 import br.com.rocko.services.PersonServices;
 
 @RestController
@@ -26,24 +25,24 @@ public class PersonController {
 	private PersonServices service;
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findByid(id);
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person)  {
+	public PersonVO create(@RequestBody PersonVO person)  {
 		return service.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person)  {
+	public PersonVO update(@RequestBody PersonVO person)  {
 		return service.update(person);
 	}
 
--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/repositories/PersonRepository.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/repositories/PersonRepository.java
@@ -1,9 +1,7 @@
 package br.com.rocko.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.rocko.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}

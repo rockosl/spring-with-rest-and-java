8dcd662388d8c8c4f6b8e816476cce6a6b7d5feb
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-03-20 09:53:58 -0300

    incluido media type yml e correcao pkey postgres

--- 05_spring-with-rest-and-java/resources/db/migration/V1__Create_Table_Person.sql
+++ 05_spring-with-rest-and-java/resources/db/migration/V1__Create_Table_Person.sql
@@ -1,7 +1,9 @@
 CREATE TABLE public.person (
-    id bigint NOT NULL,
+    id SERIAL NOT NULL,
     first_name character varying(80) NOT NULL,
     last_name character varying(80) NOT NULL,
     address character varying(100) NOT NULL,
-    gender character varying(6) NOT NULL
+    gender character varying(6) NOT NULL,
+    
+    CONSTRAINT person_pkey PRIMARY KEY (id)
 );
--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/controllers/PersonController.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.rocko.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,33 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocko.data.vo.v1.PersonVO;
 import br.com.rocko.services.PersonServices;
+import br.com.rocko.util.MediaType;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-	
+
 	@Autowired
 	private PersonServices service;
-	
-	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+
+	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML })
 	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findByid(id);
 	}
-	
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
-	
-	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public PersonVO create(@RequestBody PersonVO person)  {
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML })
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
-	
-	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public PersonVO update(@RequestBody PersonVO person)  {
+
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML })
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
 

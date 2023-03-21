68049541fa933ef91b307ed7f93e811043d5362c
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-03-21 10:19:15 -0300

    adicionado swagger para documentação

---
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.rocko.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info().title("RESTful API with java 18 and Spring Boot 3").version("v1")
+				.description("Some description about your API")
+				.termsOfService("https://github.com/rockosl/spring-with-rest-and-java")
+				.license(new License().name("Apache 2.0").url("https://github.com/rockosl/spring-with-rest-and-java")));
+	}
+
+}

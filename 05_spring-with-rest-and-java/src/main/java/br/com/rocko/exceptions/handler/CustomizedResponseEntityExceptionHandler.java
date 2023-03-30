c8645588e744bea7b43825144f282d0e0d1474c0
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-03-30 10:44:45 -0300

    Implementado jwt token

--- 05_spring-with-rest-and-java/src/main/java/br/com/rocko/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 05_spring-with-rest-and-java/src/main/java/br/com/rocko/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.rocko.exceptions.ExceptionResponse;
+import br.com.rocko.exceptions.InvalidJwtAuthenticationException;
 import br.com.rocko.exceptions.RequiredObjectIsNullException;
 import br.com.rocko.exceptions.ResourceNotFoundException;
 
@@ -35,14 +36,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(RequiredObjectIsNullException.class)
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
-		
+
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
-		
+
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 
 }

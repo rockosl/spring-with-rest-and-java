5201723b5872e5b1237f5d1fe5ea6f12c02b598f
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2023-04-08 12:46:44 -0300

    Corrigido sql para executar testar

--- 05_spring-with-rest-and-java/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ 05_spring-with-rest-and-java/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (user_name, full_name, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES
-	('leandro', 'Leandro Costa', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro Costa', '2835a3e109444baa3dd7cae44edad67ff9a537957cc374f01bd00edc2be71052175452447a23ce7d', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', 'f5299bead14e2a492cf33edab00e040695d4f2d915e4cdab38de6e1184f4a95fedb9995a7b973356', b'1', b'1', b'1', b'1');
